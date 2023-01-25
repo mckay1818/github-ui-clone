3339c2805159ec84bec16ba64e82031554918675
mckay <mckaybwarren@gmail.com>
2023-01-25 09:39:14 -0500

    create HTML skeleton with React components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import NavigationBar from "./components/NavigationBar";
+import SearchBar from "./components/SearchBar";
+import LoginControls from "./components/LoginControls";
+import HeroSection from "./components/HeroSection";
+import NewsletterForm from "./components/NewsletterForm";
+import StatsBar from "./components/StatsBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="landing-page">
+      <header>
+        <NavigationBar></NavigationBar>
+        <SearchBar></SearchBar>
+        <LoginControls></LoginControls>
       </header>
+      <main>
+        <HeroSection></HeroSection>
+        <NewsletterForm></NewsletterForm>
+      </main>
+      <footer>
+        <StatsBar></StatsBar>
+      </footer>
     </div>
   );
 }
